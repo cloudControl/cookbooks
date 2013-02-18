9a6b12865116935352c688e3f3583de710ce3c54
Tobias Wilken <tw@cloudcontrol.de>
2013-02-18 09:47:12 +0100

    logentries: add kern.log

--- logentries/attributes/default.rb
+++ logentries/attributes/default.rb
@@ -1,3 +1,4 @@
 default[:logentries][:logs][:syslog] = { "filename" => '/var/log/syslog', 'name' => 'syslog', 'type' => 'syslog' }
 default[:logentries][:logs][:chefclient] = { "filename" => '/var/log/chef/client.log', 'name' => 'chef-client.log', 'type' => 'syslog' }
 default[:logentries][:logs][:cloudinit] = { "filename" => '/var/log/cloud-init.log', 'name' => 'cloud-init.log', 'type' => 'syslog' }
+default[:logentries][:logs][:kern] = { "filename" => '/var/log/kern.log', 'name' => 'kern.log', 'type' => 'syslog' }
