a4d3b321d22b189b438e5c245c601af136b7faf1
Dimitris Verraros <vervas.g@gmail.com>
2013-07-08 15:35:28 +0200

    Remove serverdensity cookbook - cookbook shall be more generic

--- serverdensity/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default[:serverdensity][:mongodb_server] = ''
-default[:serverdensity][:mongodb_dbstats] = 'no'
-default[:serverdensity][:mongodb_replset] = 'no'
-default[:serverdensity][:nginx_status_url] = 'http://www.example.com/nginx_status'
-default[:serverdensity][:plugin_directory] = '/etc/sd-agent/plugins'
--- serverdensity/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer "Tobias Wilken"
-maintainer_email "tw@cloudcontrol.de"
-license "Apache 2.0"
-description "Installs and configures serverdensity"
-version "0.1.4"
-depends "apt"
-recipe "serverdensity::default", "Downloads, install and configures serverdensity"
